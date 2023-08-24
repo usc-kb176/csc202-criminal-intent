cdfc9ed165a2d3d44f809be93bf5d54d5a258a83
Ming <mwang@usc.edu.au>
2023-08-24 20:31:52 +1000

    Chapter 12 implementation.

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,21 +1,27 @@
 package com.example.criminalintent
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
+    private val crimeRepository = CrimeRepository.get()
+
+    private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
+    val crimes: StateFlow<List<Crime>>
+        get() = _crimes.asStateFlow()
 
     init {
-        for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title ="Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0
-            )
-            crimes += crime
+        viewModelScope.launch {
+            crimeRepository.getCrimes().collect {
+                _crimes.value = it
+            }
         }
     }
 }
+
---
+++ app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,16 @@
+package com.example.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.criminalintent.Crime
+import kotlinx.coroutines.flow.Flow
+import java.util.UUID
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): Flow<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    suspend fun getCrime(id: UUID): Crime
+}
